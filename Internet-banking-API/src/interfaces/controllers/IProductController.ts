71634ca05a81d3a46bb28587c33b1ff1a4e37aad
ImRLopezAg <angel178293lopez@gmail.com>
2023-07-15 19:54:45 -0400

    add getByOwner Method

--- Internet-banking-API/src/interfaces/controllers/IProductController.ts
+++ Internet-banking-API/src/interfaces/controllers/IProductController.ts
@@ -4,4 +4,5 @@ import { IGenericController } from '../../utils/constants'
 export interface IProductController extends IGenericController {
   GetByPin: (req: Request, res: Response, next: NextFunction) => Promise<Response>
   AddBalance: (req: Request, res: Response, next: NextFunction) => Promise<Response>
+  GetByOwner: (req: Request, res: Response, next: NextFunction) => Promise<Response>
 }
--- Internet-banking-API/src/interfaces/services/IProductService.ts
+++ Internet-banking-API/src/interfaces/services/IProductService.ts
@@ -4,4 +4,5 @@ import { IGenericService } from '../../utils/constants'
 export interface IProductService extends IGenericService<Product> {
   GetByPin: (pin: string) => Promise<Product | null>
   AddBalance: (id: string, balance: number) => Promise<Product>
+  GetByOwner: (owner: string) => Promise<Product[]>
 }
