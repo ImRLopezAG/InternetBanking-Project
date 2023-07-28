08a3d112ec4cad8ef914024f9b52dc2a0113816a
ImRLopezAG <angel178293lopez@gmail.com>
2023-07-28 15:05:01 -0700

    clean directories

--- Internet-banking-API/dist/src/presentation/middleware/error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (err, _req, res, _next) => {
-    return res.status(500).send({ message: err.message });
-};
-exports.errorHandler = errorHandler;
