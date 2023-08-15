955d899bba2b5015f0f8d8e6916ee5ce8084be13
Angel Gabriel Lopez <angel178293lopez@gmail.com>
2023-08-15 11:26:33 -0400

    fix beneficiary results

--- Internet-banking-API/src/apps/services/beneficiary.service.ts
+++ Internet-banking-API/src/apps/services/beneficiary.service.ts
@@ -1,13 +1,25 @@
-import { UserRole } from '../../utils'
 import { Beneficiary, BeneficiaryModel, User, UserModel } from '../../domain'
 import { IBeneficiaryService } from '../../interfaces'
+import { UserRole } from '../../utils'
 import { GenericService } from '../core'
 export class BeneficiaryService extends GenericService<Beneficiary> implements IBeneficiaryService {
   constructor () {
     super(BeneficiaryModel)
   }
 
+  override async Create (beneficiary: Beneficiary): Promise<Beneficiary> {
+    const { sender, receptor } = beneficiary
+    return await BeneficiaryModel.findOne({ sender, receptor }).exec().then(async (result) => {
+      if (result) {
+        return result
+      } else {
+        return await super.Create(beneficiary)
+      }
+    })
+  }
+
   async GetBySender (sender: string): Promise<User[]> {
     const beneficiaries = await BeneficiaryModel.find({ sender }).exec()
-    return await UserModel.find({ $and: [{ _id: { $in: beneficiaries } }, { role: UserRole.Client }] }).exec()  }
+    return await UserModel.find({ $or: [{ _id: { $in: beneficiaries } }, { role: UserRole.Client }] }).exec()
+  }
 }
